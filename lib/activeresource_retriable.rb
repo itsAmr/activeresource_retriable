8e4b192a5a60f6409f34efddf0f7f8902003be0d
Amr Mourad <amr.mourad@shopify.com>
2019-10-27 03:09:06 -0400

    Quick thought: create context classes
    Namespaced ActiveResource::RetriableBase::[context_name]
    This code generates the classes but of course we can't declare these
    classes as parent classes before they are created.

--- lib/activeresource_retriable.rb
+++ lib/activeresource_retriable.rb
@@ -2,6 +2,24 @@ require 'activeresource'
 require 'retriable'
 
 module ActiveResource
+  class Retriable
+    def self.configure
+      ::Retriable.configure do |c|
+        yield(c)
+
+        c.contexts.each do |name, config|
+          named_retriable = Class.new(RetriableBase)
+          named_retriable.instance_eval do
+            def request(method, path, *arguments)
+              ::Retriable.with_context(name) { super }
+            end
+          end
+          RetriableBase.const_set(name.to_s.capitalize, named_retriable)
+        end
+      end
+    end
+  end
+
   class RetriableBase < Base
     def self.retriable
       @@retriable_configuration = yield
